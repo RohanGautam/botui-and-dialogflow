8a608b4b0c9db74ba2ee8bfa5c4f9ba30235982b
Rohan Gautam <rohan1gautam@gmail.com>
2019-05-08 23:49:48 +0800

    api cleanup and make it return json

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # References/notes
 1. BotUI starter from their [website](https://docs.botui.org/install.html).
-2. `pip install dialogflow` for python
+2. `pip install dialogflow` and `pip install flask-restful` for python
+3. `python backend.py` to obtain an api endpoint which frontend can hopefully query
+example api call `http://127.0.0.1:5000/request/camp deadline`
--- backend.py
+++ backend.py
@@ -1,41 +1,25 @@
 import uuid
-# import dialogflow_v2 as dialogflow
 from backend.connect_dialogflow import detect_intent_text
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 
-app=Flask(__name__)
-api=Api(app)
+app = Flask(__name__)
+api = Api(app)
 
-projectId='testaction-d6d53'
+projectId = 'testaction-d6d53'
 
-users = [
-    {
-        "request": "rohan gautam",
-        "age": 42,
-        "occupation":"memeLord"
-    },
-    {
-        "request": "jSON",
-        "age": 13,
-        "occupation":"Jason's autistic sibling"
-    },
-    {
-        "request": "Jass",
-        "age": 48334,
-        "occupation":"oldest person ever"
-    }
-]
 
 class User(Resource):
-    def get(self,request):
-        '''get user'''
+    def get(self, request):
+        '''get request's response from dialogflow'''
         print(f'GOT request= {request}, processing')
-        return detect_intent_text(projectId, uuid.uuid4(),request,'en-US').query_result.fulfillment_text, 200
-        # for user in users:
-        #     if(request==user["request"]):
-        #         return user,200 # this is a tuple
-        # return "user not found", 404
+        # return in json format for extendability in the future
+        jsonFormat = {
+            'response': detect_intent_text(projectId, uuid.uuid4(
+            ), request, 'en-US').query_result.fulfillment_text
+        }
+        return jsonFormat, 200
+
 
 api.add_resource(User, "/request/<string:request>")
 app.run(debug=True)
