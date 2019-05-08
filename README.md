e6f213d03c89304c8ed6630b7face3bf6862c935
Rohan Gautam <rohan1gautam@gmail.com>
2019-05-08 18:40:52 +0800

    dialogflow works in python backend

---
+++ README.md
@@ -0,0 +1,3 @@
+# References/notes
+1. BotUI starter from their [website](https://docs.botui.org/install.html).
+2. `pip install dialogflow` for python
---
+++ backend.py
@@ -0,0 +1,32 @@
+import uuid
+import dialogflow_v2 as dialogflow
+
+def detect_intent_texts(project_id, session_id, texts, language_code):
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+
+    session_client = dialogflow.SessionsClient.from_service_account_file('keys/testaction-service-key.json')
+
+    session = session_client.session_path(project_id, session_id)
+    print('Session path: {}\n'.format(session))
+
+    for text in texts:
+        text_input = dialogflow.types.TextInput(
+            text=text, language_code=language_code)
+
+        query_input = dialogflow.types.QueryInput(text=text_input)
+
+        response = session_client.detect_intent(
+            session=session, query_input=query_input)
+
+        print('=' * 20)
+        print('Query text: {}'.format(response.query_result.query_text))
+        print('Detected intent: {} (confidence: {})\n'.format(
+            response.query_result.intent.display_name,
+            response.query_result.intent_detection_confidence))
+        print('Fulfillment text: {}\n'.format(
+            response.query_result.fulfillment_text))
+
+detect_intent_texts('testaction-d6d53',uuid.uuid4(),['Hello','camp deadline'], 'en-US')
